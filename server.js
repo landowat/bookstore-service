12c56f2937134e8d367e242b40d303cdf86c81a8
landowat <lando.wat@gmail.com>
2025-01-13 15:30:35 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Bookstore Service API Node.js",
+    "/Books": "Bookstore Books",
+    "/Authors": "Bookstore Authors"
+};
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+    console.log("servidor escutando!");
+});
